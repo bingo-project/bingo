c3d3cae55398bae0cd7b7064c80e3060d9c74cc5
Brooks <brooksyang@outlook.com>
2023-08-09 23:21:09 +0800

    feat: update template

--- internal/apiserver/store/user.go
+++ internal/apiserver/store/user.go
@@ -14,7 +14,7 @@ import (
 type UserStore interface {
 	Create(ctx context.Context, user *model.UserM) error
 	Get(ctx context.Context, username string) (*model.UserM, error)
-	Update(ctx context.Context, user *model.UserM) error
+	Update(ctx context.Context, user *model.UserM, fields ...string) error
 	List(ctx context.Context, offset, limit int) (int64, []*model.UserM, error)
 	Delete(ctx context.Context, username string) error
 }
@@ -31,6 +31,17 @@ func newUsers(db *gorm.DB) *users {
 	return &users{db: db}
 }
 
+// List 根据 offset 和 limit 返回 user 列表.
+func (u *users) List(ctx context.Context, offset, limit int) (count int64, ret []*model.UserM, err error) {
+	err = u.db.Offset(offset).Limit(helper.DefaultLimit(limit)).Order("id desc").Find(&ret).
+		Offset(-1).
+		Limit(-1).
+		Count(&count).
+		Error
+
+	return
+}
+
 // Create 插入一条 user 记录.
 func (u *users) Create(ctx context.Context, user *model.UserM) error {
 	return u.db.Create(&user).Error
@@ -44,19 +55,8 @@ func (u *users) Get(ctx context.Context, username string) (user *model.UserM, er
 }
 
 // Update 更新一条 user 数据库记录.
-func (u *users) Update(ctx context.Context, user *model.UserM) error {
-	return u.db.Save(&user).Error
-}
-
-// List 根据 offset 和 limit 返回 user 列表.
-func (u *users) List(ctx context.Context, offset, limit int) (count int64, ret []*model.UserM, err error) {
-	err = u.db.Offset(offset).Limit(helper.DefaultLimit(limit)).Order("id desc").Find(&ret).
-		Offset(-1).
-		Limit(-1).
-		Count(&count).
-		Error
-
-	return
+func (u *users) Update(ctx context.Context, user *model.UserM, fields ...string) error {
+	return u.db.Select(fields).Save(&user).Error
 }
 
 // Delete 根据 username 删除数据库 user 记录.
