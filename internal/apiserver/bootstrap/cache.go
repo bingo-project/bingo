5486d0e6ea4c4089d5f5c40b4cf9dd2e405a72f8
Brooks <brooksyang@outlook.com>
2023-08-18 21:12:55 +0800

    feat: update init cache

--- internal/apiserver/bootstrap/cache.go
+++ internal/apiserver/bootstrap/cache.go
@@ -1,20 +1,33 @@
 package bootstrap
 
 import (
+	"sync"
+
+	"github.com/bingo-project/component-base/cache"
 	"github.com/bingo-project/component-base/log"
 	"github.com/bingo-project/component-base/redis"
 
-	"bingo/internal/apiserver/cache"
 	"bingo/internal/apiserver/facade"
 )
 
+var (
+	once   sync.Once
+	prefix = "cache:"
+)
+
 func InitCache() {
-	r, err := redis.NewClient(facade.Config.Redis.Host, facade.Config.Redis.Password, facade.Config.Redis.Database)
+	rds, err := redis.NewClient(facade.Config.Redis.Host, facade.Config.Redis.Password, facade.Config.Redis.Database)
 	if err != nil {
 		log.Errorw("init cache failed", "err", err)
 
 		return
 	}
 
-	cache.NewCache(r)
+	once.Do(func() {
+		facade.Redis = rds.Client
+		facade.Cache = cache.NewService(&cache.RedisStore{
+			RedisClient: rds,
+			KeyPrefix:   prefix,
+		})
+	})
 }
