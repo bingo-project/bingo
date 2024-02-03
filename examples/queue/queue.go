82ad2ad3e408a29af3cb57821b4c5f39109e3f3d
Brooks <brooksyang@outlook.com>
2024-02-03 19:35:19 +0800

    feat: add task example

--- examples/queue/queue.go
+++ examples/queue/queue.go
@@ -1,1 +1,36 @@
-package queue
+package main
+
+import (
+	"log"
+
+	"github.com/duke-git/lancet/v2/convertor"
+	"github.com/hibiken/asynq"
+
+	"bingo/internal/apiserver/job"
+)
+
+var (
+	Queue *asynq.Client
+)
+
+func init() {
+	Queue = asynq.NewClient(asynq.RedisClientOpt{Addr: ":6379", DB: 1})
+}
+
+func main() {
+	// Enqueue task
+	payload := &job.EmailTaskPayload{
+		Username: "Peter",
+		Email:    "peter@gmail.com",
+	}
+
+	t := asynq.NewTask("demo:task", []byte(convertor.ToString(payload)))
+	info, err := Queue.Enqueue(t)
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	log.Println("demo:task enqueued", convertor.ToString(info))
+}
--- internal/apiserver/job/job.go
+++ internal/apiserver/job/job.go
@@ -4,4 +4,5 @@ import "github.com/hibiken/asynq"
 
 func AddJobs(mux *asynq.ServeMux) {
 	// Add jobs here
+	mux.HandleFunc("demo:task", HandleWelcomeEmailTask) // Demo task
 }
