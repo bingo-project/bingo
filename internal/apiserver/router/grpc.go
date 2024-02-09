f00557313c9bf2d7098543a955fd99b8212e8ee3
Brooks <brooksyang@outlook.com>
2024-02-09 17:34:27 +0800

    feat: add grpc service

--- internal/apiserver/router/grpc.go
+++ internal/apiserver/router/grpc.go
@@ -1,7 +1,14 @@
 package router
 
-import "google.golang.org/grpc"
+import (
+	"google.golang.org/grpc"
+
+	"bingo/internal/apiserver/grpc/controller/v1/apiserver"
+	v1 "bingo/internal/apiserver/grpc/proto/v1"
+	"bingo/internal/apiserver/store"
+)
 
 func GRPC(g *grpc.Server) {
-	//
+	// ApiServer
+	v1.RegisterApiServerServer(g, apiserver.New(store.S))
 }
--- scripts/make-rules/generate.mk
+++ scripts/make-rules/generate.mk
@@ -21,7 +21,6 @@ gen.protoc: tools.verify.protoc-gen-go ## 编译 protobuf 文件.
 	@echo "===========> Generate protobuf files"
 	@protoc                                            \
 		--proto_path=$(APIROOT)                          \
-		--proto_path=$(ROOT_DIR)/third_party             \
 		--go_out=paths=source_relative:$(APIROOT)        \
 		--go-grpc_out=paths=source_relative:$(APIROOT)   \
 		$(shell find $(APIROOT) -name *.proto)
