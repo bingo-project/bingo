4ea446c3021d8f42084c26397239504cbc976847
Brooks <brooksyang@outlook.com>
2024-05-21 21:50:04 +0800

    feat: add app tables

---
+++ internal/bingoctl/database/migration/2024_05_21_205542_create_app_table.go
@@ -0,0 +1,36 @@
+package migration
+
+import (
+	"gorm.io/gorm"
+
+	"github.com/bingo-project/bingoctl/pkg/migrate"
+
+	"bingo/internal/apiserver/model"
+)
+
+type CreateAppTable struct {
+	model.Base
+
+	UID         string `gorm:"type:varchar(255);index:idx_uid;not null;default:''"`
+	AppID       string `gorm:"type:varchar(255);uniqueIndex:uk_app_id;not null;default:''"`
+	Name        string `gorm:"type:varchar(255);not null;default:''"`
+	Status      string `gorm:"type:tinyint;not null;default:1;comment:Status, 1-enabled, 2-disabled"`
+	Description string `gorm:"type:varchar(1000);not null;default:''"`
+	Logo        string `gorm:"type:varchar(1000);not null;default:''"`
+}
+
+func (CreateAppTable) TableName() string {
+	return "app"
+}
+
+func (CreateAppTable) Up(migrator gorm.Migrator) {
+	_ = migrator.AutoMigrate(&CreateAppTable{})
+}
+
+func (CreateAppTable) Down(migrator gorm.Migrator) {
+	_ = migrator.DropTable(&CreateAppTable{})
+}
+
+func init() {
+	migrate.Add("2024_05_21_205542_create_app_table", CreateAppTable{}.Up, CreateAppTable{}.Down)
+}
---
+++ internal/bingoctl/database/migration/2024_05_21_210642_create_app_api_key_table.go
@@ -0,0 +1,41 @@
+package migration
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+
+	"github.com/bingo-project/bingoctl/pkg/migrate"
+
+	"bingo/internal/apiserver/model"
+)
+
+type CreateAppApiKeyTable struct {
+	model.Base
+
+	UID         string    `gorm:"type:varchar(255);index:idx_uid;not null;default:''"`
+	AppID       string    `gorm:"type:varchar(255);index:idx_app_id;not null;default:''"`
+	Name        string    `gorm:"type:varchar(255);not null;default:''"`
+	AccessKey   string    `gorm:"type:varchar(255);not null;default:''"`
+	SecretKey   string    `gorm:"type:varchar(255);not null;default:''"`
+	Status      string    `gorm:"type:tinyint;not null;default:1;comment:Status, 1-enabled, 2-disabled"`
+	ACL         string    `gorm:"type:json;default:null"`
+	Description string    `gorm:"type:varchar(1000);not null;default:''"`
+	ExpiredAt   time.Time `gorm:"type:DATETIME(3);index:idx_expired_at"`
+}
+
+func (CreateAppApiKeyTable) TableName() string {
+	return "app_api_key"
+}
+
+func (CreateAppApiKeyTable) Up(migrator gorm.Migrator) {
+	_ = migrator.AutoMigrate(&CreateAppApiKeyTable{})
+}
+
+func (CreateAppApiKeyTable) Down(migrator gorm.Migrator) {
+	_ = migrator.DropTable(&CreateAppApiKeyTable{})
+}
+
+func init() {
+	migrate.Add("2024_05_21_210642_create_app_api_key_table", CreateAppApiKeyTable{}.Up, CreateAppApiKeyTable{}.Down)
+}
