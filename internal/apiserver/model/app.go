db88148e92c6a9ff8ba14c5804c1f9588d26d290
Brooks <brooksyang@outlook.com>
2024-09-06 21:29:05 +0800

    feat: update app model

--- internal/apiserver/model/app.go
+++ internal/apiserver/model/app.go
@@ -7,19 +7,27 @@ import (
 )
 
 type App struct {
-	gorm.Model
+	Base
 
-	UID         string `gorm:"column:uid;type:varchar(255);not null;index:idx_uid,priority:1" json:"uid"`
-	AppID       string `gorm:"column:app_id;type:varchar(255);not null;uniqueIndex:uk_app_id,priority:1" json:"appId"`
-	Name        string `gorm:"column:name;type:varchar(255);not null" json:"name"`
-	Status      *int32 `gorm:"column:status;type:tinyint;not null;default:1;comment:Status, 1-enabled, 2-disabled" json:"status"` // Status, 1-enabled, 2-disabled
-	Description string `gorm:"column:description;type:varchar(1000);not null" json:"description"`
-	Logo        string `gorm:"column:logo;type:varchar(1000);not null" json:"logo"`
+	UID         string    `gorm:"column:uid;type:varchar(255);not null;index:idx_uid,priority:1" json:"uid"`
+	AppID       string    `gorm:"column:app_id;type:varchar(255);not null;uniqueIndex:uk_app_id,priority:1" json:"appId"`
+	Name        string    `gorm:"column:name;type:varchar(255);not null" json:"name"`
+	Status      AppStatus `gorm:"column:status;type:tinyint;not null;default:1;comment:Status, 1-enabled, 2-disabled" json:"status"` // Status, 1-enabled, 2-disabled
+	Description string    `gorm:"column:description;type:varchar(1000);not null" json:"description"`
+	Logo        string    `gorm:"column:logo;type:varchar(1000);not null" json:"logo"`
 }
 
 func (*App) TableName() string {
 	return "app"
 }
+
+// AppStatus 1-enabled, 2-disabled.
+type AppStatus int
+
+const (
+	AppStatusEnabled  AppStatus = 1
+	AppStatusDisabled AppStatus = 2
+)
 
 func (u *App) BeforeCreate(tx *gorm.DB) (err error) {
 	// Generate App id
