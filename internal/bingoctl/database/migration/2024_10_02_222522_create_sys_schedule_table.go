364257072d7d9ab0c745884e2609f547f3e505d7
Brooks <brooksyang@outlook.com>
2024-10-02 22:28:56 +0800

    feat: add schedule table

---
+++ internal/bingoctl/database/migration/2024_10_02_222522_create_sys_schedule_table.go
@@ -0,0 +1,35 @@
+package migration
+
+import (
+	"gorm.io/gorm"
+
+	"github.com/bingo-project/bingoctl/pkg/migrate"
+
+	"bingo/internal/apiserver/model"
+)
+
+type CreateSysScheduleTable struct {
+	model.Base
+
+	Name        string `gorm:"type:varchar(255);not null;default:''"`
+	Job         string `gorm:"type:varchar(255);not null;default:''"`
+	Spec        string `gorm:"type:varchar(255);not null;default:''"`
+	Status      string `gorm:"type:tinyint;not null;default:1;comment:Status, 1-enabled, 2-disabled"`
+	Description string `gorm:"type:varchar(1000);not null;default:''"`
+}
+
+func (CreateSysScheduleTable) TableName() string {
+	return "sys_schedule"
+}
+
+func (CreateSysScheduleTable) Up(migrator gorm.Migrator) {
+	_ = migrator.AutoMigrate(&CreateSysScheduleTable{})
+}
+
+func (CreateSysScheduleTable) Down(migrator gorm.Migrator) {
+	_ = migrator.DropTable(&CreateSysScheduleTable{})
+}
+
+func init() {
+	migrate.Add("2024_10_02_222522_create_sys_schedule_table", CreateSysScheduleTable{}.Up, CreateSysScheduleTable{}.Down)
+}
