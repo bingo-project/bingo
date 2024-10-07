595ddfc982ba06627ffe06b7ed4d71f5aacf0226
Brooks <brooksyang@outlook.com>
2024-10-07 12:04:37 +0800

    refactor: grpc interceptor directory

--- internal/apiserver/grpc.go
+++ internal/apiserver/grpc.go
@@ -9,9 +9,9 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	"bingo/internal/apiserver/grpc/interceptor"
 	"bingo/internal/apiserver/router"
 	"bingo/internal/pkg/facade"
+	"bingo/internal/pkg/grpc/interceptor"
 )
 
 type grpcAPIServer struct {
