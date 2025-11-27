1e9d6d7b246cef0635ac14f612be98d00191c940
Brooks <brooksyang@outlook.com>
2025-11-27 22:48:43 +0800

    feat: update bingoctl config file

--- internal/bingoctl/cmd/cmd.go
+++ internal/bingoctl/cmd/cmd.go
@@ -75,7 +75,7 @@ func NewBingoCtlCommand(in io.Reader, out, err io.Writer) *cobra.Command {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	bootstrap.InitConfig("bingo-admserver.yaml")
+	bootstrap.InitConfig("bingoctl.yaml")
 	bootstrap.Boot()
 
 	// Init migration
