2fc3129c5a579a2c1ed3201fb00e9b74026494ab
Brooks <brooksyang@outlook.com>
2025-12-28 19:48:32 +0800

    feat(task): add announcement publish task type

--- internal/pkg/task/types.go
+++ internal/pkg/task/types.go
@@ -1,11 +1,16 @@
 package task
 
 const (
-	EmailVerificationCode = "email:verification"
+	EmailVerificationCode   = "email:verification"
+	AnnouncementPublish     = "announcement:publish"
 )
 
 type EmailVerificationCodePayload struct {
 	To      string
 	Subject string
 	Content string
+}
+
+type AnnouncementPublishPayload struct {
+	AnnouncementID uint64 `json:"announcement_id"`
 }
